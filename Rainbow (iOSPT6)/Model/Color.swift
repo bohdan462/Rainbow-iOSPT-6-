4985547605d49eb51654b2471f3393b754e05be4
bohdan462 <tech.bohdan@gmail.com>
2020-04-07 21:34:49 -0500

    set up color model

---
+++ Rainbow (iOSPT6)/Model/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Rainbow (iOSPT6)
+//
+//  Created by Bohdan Tkachenko on 4/7/20.
+//  Copyright © 2020 Bohdan Tkachenko. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    var colorName: String
+    var color: UIColor
+}
